4d5b8becb067d6962bb6b610b3f3241042743eef
Irvin Owens Jr <irvinowens@gmail.com>
2011-07-15 20:37:34 -0700

    Creating a folder for tests

--- lib/Erudite.rb
+++ lib/Erudite.rb
@@ -1,3 +1,14 @@
+require 'Erudite/discovery.rb'
+require 'Erudite/distribute.rb'
+
+# The erudite gem is a runtime that provides a data store which
+# features automatic clustering, a sql interface on port 3306,
+# a map / reduce interface ( using ruby )
+# and a file base storage mechanism for maximum flexibility
+
 module Erudite
   # Your code goes here...
+  include Disco
+  
+  
 end
---
+++ lib/Erudite/discovery.rb
@@ -0,0 +1,15 @@
+module Disco
+	class Cluster
+		def initialize
+		
+		end
+		
+		def start_pings
+			puts "Hostname #{Socket.gethostname}"
+		end
+		
+		def start_listener
+		
+		end
+	end
+end
