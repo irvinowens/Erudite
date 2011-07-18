3f78a5b32e5b0bcf6bf25abc0746a42a43b94647
Irvin Owens Jr <irvinowens@gmail.com>
2011-07-18 12:55:12 -0700

    Getting closer to writing the ping mechanism

--- lib/Erudite.rb
+++ lib/Erudite.rb
@@ -5,6 +5,12 @@ require File.dirname(__FILE__) + '/Erudite/distribute.rb'
 # features automatic clustering, a sql interface on port 3306,
 # a map / reduce interface ( using ruby )
 # and a file base storage mechanism for maximum flexibility
+# the physical architecture is a set of index files that is created
+# upon saving content and each entry is tied to the physical object
+# for text objects, all tokens are identified and tied to the location
+# of the document to make lookup quick
+# to reduce the content, add a ruby function to further limit
+# the match.
 
 module Erudite
   # Your code goes here...
--- lib/Erudite/discovery.rb
+++ lib/Erudite/discovery.rb
@@ -16,11 +16,23 @@ module Disco
 			puts "Hostname #{Socket.ip_address_list}"
 			@ip_address = Socket.ip_address_list
 			@ip_address.each do |ip_addr|
-				
+				if ip_addr.ip?
+					if ip_addr.ipv4?
+						if ip_addr.ipv4_loopback? == false
+							if ip_addr.ipv4_private? == true
+								puts "private address #{ip_addr.ip_address}"
+							end
+						end
+					end
+				end
 			end
 		end
 		
 		def start_listener
+		
+		end
+		
+		def check_range_for_ip_address ip
 		
 		end
 	end
